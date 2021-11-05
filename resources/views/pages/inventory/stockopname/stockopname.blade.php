651427c239d7dbd2ba39dc4764a7561f55b9ba04
adimertaaa <adimerta@student.unud.ac.id>
2021-11-06 00:29:55 +0800

    gg

--- resources/views/pages/inventory/stockopname/stockopname.blade.php
+++ resources/views/pages/inventory/stockopname/stockopname.blade.php
@@ -259,20 +259,45 @@ function submit1() {
             kode_opname: kode_opname
         }
 
-        if (tanggal_opname == 0 | tanggal_opname == '') {
-            $('#alerttanggal').show()
-        } else if (id_gudang == 0 | id_gudang == '')
-            $('#alertsupplier').show()
-        else {
+        if (tanggal_opname == '' | id_gudang == '' | id_gudang == 'Pilih Gudang') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Terdapat Field Data Kosong!',
+            })
+        }else {
+            var sweet_loader =
+                '<div class="sweet_loader"><svg viewBox="0 0 140 140" width="140" height="140"><g class="outline"><path d="m 70 28 a 1 1 0 0 0 0 84 a 1 1 0 0 0 0 -84" stroke="rgba(0,0,0,0.1)" stroke-width="4" fill="none" stroke-linecap="round" stroke-linejoin="round"></path></g><g class="circle"><path d="m 70 28 a 1 1 0 0 0 0 84 a 1 1 0 0 0 0 -84" stroke="#71BBFF" stroke-width="4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-dashoffset="200" stroke-dasharray="300"></path></g></svg></div>';
+
             $.ajax({
                 method: 'post',
                 url: '/inventory/Opname',
                 data: data,
+                beforeSend: function () {
+                    swal.fire({
+                        title: 'Mohon Tunggu!',
+                        html: 'Data Sedang Diproses...',
+                        showConfirmButton: false,
+                        onRender: function () {
+                            // there will only ever be one sweet alert open.
+                            $('.swal2-content').prepend(sweet_loader);
+                        }
+                    });
+                },
                 success: function (response) {
+                    swal.fire({
+                        icon: 'success',
+                        showConfirmButton: false,
+                        html: '<h5>Success!</h5>'     
+                    });
                     window.location.href = '/inventory/Opname/' + response.id_opname + '/edit'
                 },
                 error: function (error) {
                     console.log(error)
+                    swal.fire({
+                        icon: 'error',
+                        html: '<h5>Error!</h5>'
+                    });
                 }
 
             });
